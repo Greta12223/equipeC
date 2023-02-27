5e817cc4339b3e1643e26b3e72cdc4dc7ec29b59
Gaël Sudour-Jeannin <gael.sudourjeannin@gmail.com>
2023-02-27 14:49:29 +0100

    navigation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -8,7 +8,7 @@ function HomeScreen() {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen branche Gaël</Text>
       <Image
-      style ={{width: "100%", height:"50%"}}
+        style={{ width: "100%", height: "50%" }}
         source={{
           uri: 'https://media2.giphy.com/media/8xgqLTTgWqHWU/giphy.gif?cid=ecf05e47daacodjbe66288z7ytkvg8zffttng3kograj0qfc&rid=giphy.gif'
         }}
@@ -17,13 +17,61 @@ function HomeScreen() {
   );
 }
 
+function Musiques() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Musique branche Gaël</Text>
+    </View>
+  );
+}
+
+function Films() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Films branche Gaël</Text>
+    </View>
+  );
+}
+
 const Stack = createNativeStackNavigator();
 
 function App() {
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options= {({ navigation }) =>({
+          title: 'Accueil',
+          headerStyle: {
+            backgroundColor: '#65c7f8',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          }, headerRight: () => (
+
+            <>
+              <View style={{ margin: 5}}>
+                <Button
+                  onPress={() => navigation.navigate("Musiques")}
+                  title="Musiques"
+                  color="#65c7f8"
+                />
+              </View>
+
+              <View style={{ margin: 5 }}>
+                <Button
+                  onPress={() => navigation.navigate("Films")}
+                  title="Films"
+                  color="#65c7f8"
+                />
+              </View>
+            </>
+          ),
+
+        })} />
+        <Stack.Screen name="Musiques" component={Musiques}/>
+        <Stack.Screen name="Films" component={Films}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
